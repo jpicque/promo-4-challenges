693165dac17eed49a59c3d41bd1d0fd8a411f6fc
jpicque <detrituss@live.fr>
2014-10-07 14:52:19 +0200

    exercice 4

--- 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
+++ 01-Ruby/01-Programming-basics/04-What-your-name/lib/interface.rb
@@ -5,4 +5,17 @@ require_relative "compute_name"
 # TODO: ask for the last name
 # TODO: Print a greeting to the user with the full name concatenated,
 #       something like "Hello, Boris Paillard"
+puts "What\'s your first name?"
+first_name = gets.chomp
 
+
+puts "What\'s your middle name?"
+middle_name = gets.chomp
+
+
+puts "What\'s your last name?"
+last_name = gets.chomp
+
+ 
+full_name = compute_name(first_name, middle_name, last_name)
+puts "Hello, #{full_name}"
