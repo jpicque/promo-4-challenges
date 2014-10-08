699901d946543ca1912cb1465ac3d630035652ca
jpicque <detrituss@live.fr>
2014-10-08 15:29:48 +0200

    exo 2

--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/interface.rb
@@ -4,10 +4,27 @@ require_relative "wagon_sort"
 #       Remember, to read an input from the command line, use:
 #       - `gets`:  http://www.ruby-doc.org/core-2.1.2/Kernel.html#method-i-gets
 #       - `chomp`: http://www.ruby-doc.org/core-2.1.2/String.html#method-i-chomp
-puts "What's your name?"
-students = gets.chomp
-wagon_sort >> students
+liste = []
+
+puts "Type a student name"
+name = gets.chomp
+liste << name
+
+while name != ""
+	puts "Type another student name"
+	name = gets.chomp
+	if name != ""
+		liste << name
+	else
+		puts "Type another student name"
+	end
+end
+
+nombre = liste.count
+
+puts "Congratulations! Your Wagon has #{nombre} students : #{liste.to_s}"
 
 # TODO: Then call `wagon_sort` method defined in the wagon_sort.rb
 #       file and display the sorted student list
-display wagon_sort.sort
+puts wagon_sort(liste)
+
--- 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
+++ 01-Ruby/02-Arrays-Enumerable/02-Sorting-the-wagon/lib/wagon_sort.rb
@@ -1,7 +1,6 @@
-require interface.rb
-
 def wagon_sort(students)
   # TODO: return (not print!) a copy of students, sorted alphabetically
-  wagon_sort = []
-  wagon_sort << students
+  students.sort_by { |x| x.downcase }
 end
+
+
