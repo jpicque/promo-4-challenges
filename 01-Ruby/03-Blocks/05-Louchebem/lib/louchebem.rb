8150eb28a04d686cf6ec78d73a7a00e8a754a107
jpicque <detrituss@live.fr>
2014-10-10 09:17:05 +0200

    exo5

--- 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
+++ 01-Ruby/03-Blocks/05-Louchebem/lib/louchebem.rb
@@ -2,4 +2,16 @@
 
 def louchebemize(sentence)
   # TODO: implement your louchebem translator
+  if word.length > 1
+  	puts word.to_a
+  		if word.to_a.first !include ("a","e","i","u","y")
+  			puts word.to_a.first.shift
+
 end
+
+# On veut savoir si le mot a plus d'une lettre
+# On veut trouver la première consonne du mot
+# On veut déplacer cette consonne à la fin du mot
+# On veut ajouter un "l" au début du mot et "ème" à la fin du mot
+
+
