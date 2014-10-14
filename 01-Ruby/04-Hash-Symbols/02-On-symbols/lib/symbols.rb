a3163a71d0cb911d26ce27df9468b14946ced19c
jpicque <detrituss@live.fr>
2014-10-14 23:48:14 +0200

    zzz

--- 01-Ruby/04-Hash-Symbols/02-On-symbols/lib/symbols.rb
+++ 01-Ruby/04-Hash-Symbols/02-On-symbols/lib/symbols.rb
@@ -1,36 +1,40 @@
- 
- def are_identical_symbols_same_objects?
-   # TODO: true or false?
+# NOTE: Read about BasicObject#object_id
+# http://www.ruby-doc.org/core-2.1.1/BasicObject.html#method-i-__id__
+
+# You can try launching `irb` to test some values
+# irb> :foo.object_id
+# irb> :foo.object_id
+# irb> "foo".object_id
+# irb> "foo".object_id
+
+def are_identical_symbols_same_objects?
   true
- end
- 
- def are_identical_strings_same_objects?
-   # TODO: true or false?
+end
+
+def are_identical_strings_same_objects?
   false
- end
- 
- 
- @@ -22,16 +24,23 @@ def are_identical_strings_same_objects?
- 
- def convert_string_to_symbol(a_string)
-   # TODO: return the symbol version of "a_string"
+end
+
+
+# Remember, RTFM! Your doc is your friend
+# - http://www.ruby-doc.org/core-2.1.1/String.html
+# - http://www.ruby-doc.org/core-2.1.1/Symbol.html
+
+def convert_string_to_symbol(a_string)
   a_string.to_sym
- end
- 
- def convert_symbol_to_string(a_symbol)
-   # TODO: return the string version of "a_symbol"
+end
+
+def convert_symbol_to_string(a_symbol)
   a_symbol.to_s
- end
- 
- def me
-   # TODO: return a Hash representing yourself, with keys such as age and name
-  myself = {
-    age: 27,
-    name: "Julien"
+end
+
+def me
+  {
+    name: "Boris",
+    age: 26
   }
- end
- 
- def fruits
-   # TODO: return an array of fruits
-    array = ["pomme", "banane", "fraise"]
- end
+end
+
+def fruits
+  %w(apple banana orange)
+end
