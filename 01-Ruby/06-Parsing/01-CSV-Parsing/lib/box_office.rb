ebceec6390a78f76bd348287302c6b24404b1c69
jpicque <detrituss@live.fr>
2014-10-14 18:54:50 +0200

    ex1

--- 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
+++ 01-Ruby/06-Parsing/01-CSV-Parsing/lib/box_office.rb
@@ -7,18 +7,28 @@ require 'csv'
 
 def most_successfull(number, max_year, file_name)
   # TODO: return the number most successfull movies max_year
-array_movies = []
+movies = []
 csv_options = { col_sep: ','}
 filepath = "/Users/julien/code/jpicque/promo-4-challenges/01-Ruby/06-Parsing/01-CSV-Parsing/lib/movies.csv"
 
-	CSV.foreach(file_name, csv_options) do |row|
-		array_movies << {name: row[0], year: row[1].to_i, earnings: row[2].to_i}
-		array_movies = array_movies.sort {|a, b| b[:earnings] <=> a[:earnings]}
-	end	
-	
-	array_movies = array_movies.select { |movie| movie[:year] < max_year }
-	array_movies = array_movies[0...number]
-return array_movies
+CSV.foreach(file_name, csv_options) do |row|
+	movie = {
+		:name => row[0],	
+		:year => row[1].to_i,	
+		:earnings => row[2].to_i		
+	}
+		movies << movie
+		end
+movies_date = []
+movies.each do |movie|
+	if movie[:year] < max_year
+	movies_date << movie
+	end
+end
+movies_date = movies_date.sort_by { |movie| - movie[:earnings]}
+# On met un moins pour lui dire de faire dans l'ordre décroissant car sinon il fait dans l'ordre croissant.
+movies_date.take(number)
+
 end
 
 
