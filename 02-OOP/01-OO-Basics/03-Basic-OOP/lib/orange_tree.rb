040fbfc0f7078860ea10fb7aa3888ee5589e13b2
jpicque <detrituss@live.fr>
2014-10-15 16:54:37 +0200

    aaa

--- 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
+++ 02-OOP/01-OO-Basics/03-Basic-OOP/lib/orange_tree.rb
@@ -1,48 +1,42 @@
 class OrangeTree
-# TODO: Implement all the specs defined in the README.md :)
+  # TODO: Implement all the specs defined in the README.md :)
   attr_accessor :height, :age, :fruits
 
-    def initialize
-      @height = 0
-      @age = 0
+  def initialize
+    @height = 0
+    @age = 0
+    @fruits = 0
+  end
+
+  def one_year_passes!
+    @age += 1
+    @height += 1 if @age <= 10
+    fruits_counter
+  end
+
+  def fruits_counter
+    if @age >= 0 && @age <= 5
+      @fruits = 0
+    elsif @age > 5 && @age <= 10
+      @fruits = 100
+    elsif @age > 10 && @age <= 15
+      @fruits = 200
+    else
       @fruits = 0
     end
+  end
 
-    def one_year_passes!
-      @age += 1
-  	  @height += 1 if @age <= 10
-  	  if @age >= 0 && @age <=5
-        @fruits = 0
-      elsif @age > 5 && @age <= 10
-  		  @fruits = 100
-  	  elsif @age > 10 && @age <= 15
-  		  @fruits = 200
-  	  else
-  		  @fruits = 0
-  	  end	
+  def pick_a_fruit!
+    @fruits -= 1
+  end
+
+  def dead?
+    if @age >= 100
+      true
+    elsif @age > 50
+      [true, false].sample
+    else
+      false
     end
-
-    def pick_a_fruit!
-  	  @fruits -= 1
-    end
-
-    def dead?
-  	  if @age >= 100
-        true
-      elsif @age > 50
-        [true, false].sample
-      else
-        false
-      end
-    end
-
-end
-
-toto = OrangeTree.new
-
-5.times do 
-	toto.one_year_passes!
-	p toto.age
-	p toto.fruits
-	
+  end
 end
