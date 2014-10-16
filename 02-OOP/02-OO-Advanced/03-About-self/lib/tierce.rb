9ddf3b63ce01f65cd300e420f94608bcb0aed0aa
jpicque <detrituss@live.fr>
2014-10-16 15:52:39 +0200

    ex3

--- 02-OOP/02-OO-Advanced/03-About-self/lib/tierce.rb
+++ 02-OOP/02-OO-Advanced/03-About-self/lib/tierce.rb
@@ -2,21 +2,24 @@ module DemoModule
   class DemoClass
     def self.introduce
       # TODO: choose return between 1, 2, 3
+      2
     end
 
     def introduce
       # TODO: choose return between 1, 2, 3
+      3
     end
   end
 
   def self.introduce
     # TODO: choose return between 1, 2, 3
+    1
   end
 end
 
 def tierce?
-  first = DemoModule.introduce
-  second = DemoModule::DemoClass.introduce
-  third = DemoModule::DemoClass.new.introduce
+  first = DemoModule.introduce # Car il ne fait pas appelle a DemoClass
+  second = DemoModule::DemoClass.introduce # Car self est une methode de classe
+  third = DemoModule::DemoClass.new.introduce # Car new nous indique que l'on cree une instance
   [first, second, third] == [1, 2, 3]
 end
