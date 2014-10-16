0247702e28c95c51473f5db2e8bf66b3f494db8b
jpicque <detrituss@live.fr>
2014-10-16 14:27:52 +0200

    ex2

--- 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
+++ 02-OOP/02-OO-Advanced/02-Instance-vs-Class/lib/restaurant.rb
@@ -1,9 +1,32 @@
 class Restaurant
   # TODO: add relevant accessors if necessary
+  attr_reader :name, :city
+  attr_accessor :ratings, :average_rating
 
   def initialize(city, name)
     # TODO: implement constructor with relevant instance variables
+    @city = city
+    @name = name
+    @ratings = []
+    @average_rating = 0
+  end
+
+  # Calculer la moyenne de toutes les notes attribuees a un restaurant
+  def rate(rating)
+    @ratings << rating
+    sum = 0
+    @ratings.each { |note| sum += note }
+    @average_rating = sum / @ratings.count
   end
 
   # TODO: implement #filter_by_city and #rate methods
+  def self.filter_by_city(restaurants, city)
+    restos = []
+    restaurants.each do |resto|
+      restos << resto if resto.city == city
+    end
+    return restos
+  end
 end
+
+p Restaurant.new("Lyon", "bocuse")
