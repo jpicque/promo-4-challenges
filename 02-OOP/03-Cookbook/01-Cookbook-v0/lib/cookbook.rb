33ccffb2ce77b282a810d49d30971738ba5465fd
jpicque <detrituss@live.fr>
2014-10-20 09:07:12 +0200

    cookbook

--- 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
+++ 02-OOP/03-Cookbook/01-Cookbook-v0/lib/cookbook.rb
@@ -1,30 +1,33 @@
-# require_relative 'recipes_csv'
-require 'csv'
+# Encoding: utf-8
+require "csv"
+require_relative "controller"
+require_relative "recipe"
 
 class Cookbook
+  attr_reader :recipes
 
- attr_reader :recipes
+  def initialize(csv_file)
+    @recipes = []
+    @csv_file = csv_file
 
- def initialize(csv_path)
-   @recipes = []
-   @csv_path = csv_path
-   csv_options = { col_sep: ',', quote_char: '"' }
-   CSV.foreach(csv_path, csv_options) do |row|
-     @recipes << Recipe.new(:name, :description)
-   end
- end
-
- def add_recipe(recipe)
- 	@recipes << recipe
-    csv_options = { col_sep: ',', quote_char: '"' }
-    CSV.open(@csv_path, 'a+', csv_options) do |csv|
-    	csv << [recipe.name, recipe.description]
+    csv_options = { col_sep: ',', encoding: "utf-8" }
+    CSV.foreach(csv_file, csv_options) do |row|
+      @recipes << Recipe.new(row[0], row[1])
     end
- end
+  end
 
- def remove_recipe(index)
- 		@recipes.delete_at(index)
- 		@recipes
-
- end
+  def add_recipe(recipe)
+    @recipes << recipe
+    CSV.open(@csv_file, "wb") do |csv|
+      @recipes.each { |recipe| csv << [recipe.name, recipe.description] }
+    end
+  end
+def remove_recipe(index)
+   @recipes.delete_at(index)
+   CSV.open(@csv_file, 'w') do |csv|
+     @recipes.each do |recipe|
+       csv << [recipe.name, recipe.description]
+     end
+   end
+end
 end
