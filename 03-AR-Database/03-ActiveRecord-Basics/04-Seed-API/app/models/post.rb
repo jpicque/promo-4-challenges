7d315eed72d8954e912ef5cdabcd9c5c004027a8
jpicque <detrituss@live.fr>
2014-10-27 18:05:00 +0100

    ex4

--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/app/models/post.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/app/models/post.rb
@@ -1,2 +1,4 @@
+require "active_record"
+
 class Post < ActiveRecord::Base
 end
--- 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/04-Seed-API/db/seeds.rb
@@ -1,4 +1,18 @@
 require "json"
 require "rest-client"
-
 # TODO: Write a seed to insert 100 posts in the database fetched from the Hacker News API.
+
+table_data = RestClient.get 'https://hacker-news.firebaseio.com/v0/topstories.json'
+table_data = JSON.parse(table_data)
+
+table_data_top = table_data[0..10]
+p table_data_top
+
+table_data_top.each do |id|
+  a =  RestClient.get 'http://api.ihackernews.com/post/#{id}.json'
+  p a
+  b = JSON.parse(a)
+  p b
+  post = Post.new(name: b[:title], url: b[:url], votes: b[:votes])
+  post.save
+end
