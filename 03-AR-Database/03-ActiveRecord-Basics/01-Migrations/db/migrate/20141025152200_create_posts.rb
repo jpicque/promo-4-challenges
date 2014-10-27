c91688302db578b4687a8b53e743e6e347c956e6
jpicque <detrituss@live.fr>
2014-10-27 11:53:05 +0100

    ex1

--- 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
+++ 03-AR-Database/03-ActiveRecord-Basics/01-Migrations/db/migrate/20141025152200_create_posts.rb
@@ -1,5 +1,10 @@
 class CreatePosts < ActiveRecord::Migration
   def change
     # TODO: your code here to create the posts table
+    create_table :posts do |p|
+      p.string :name
+      p.string :url
+      p.timestamps
+    end
   end
 end
