b34264ad4f8678f1805a075b3e7994a7050ec979
jpicque <detrituss@live.fr>
2014-10-28 13:40:41 +0100

    ex2

--- 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
+++ 03-AR-Database/04-ActiveRecord-Advanced/02-Validations/app/models/user.rb
@@ -1,5 +1,12 @@
 class User < ActiveRecord::Base
   has_many :posts
-
   # TODO: Add some validation
+  before_validation :stripspaces
+  validates :username, :email, presence: true
+  validates :username, uniqueness: true
+  validates :email, format: { with: /\A\w+@\w+\.\w{2,3}\z/, message: "invalid email" }
+end
+
+def stripspaces
+  self.email = email.lstrip.rstrip unless email.nil?
 end
