2436acf9227d83c9c0ee0df2c5e80c5a710d52f5
jpicque <detrituss@live.fr>
2014-11-03 15:19:23 +0100

    ex2

--- 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
+++ 04-Front-End/04-Javascript-jQuery/02-Events-Animation/application.js
@@ -3,3 +3,15 @@
   * TODO: add your code here!
   *
 */
+
+// Faire apparaitre le pop-up
+$(function() {
+  $('#message').slideDown('slow');
+  $('#message').delay(1000);
+  $('#message').on('click', function(e) {
+    $(message).slideUp('slow');
+  });
+  $('#message-wrapper').on("mouseenter", function(e){
+    $("#message").slideDown(500);
+  });
+});
